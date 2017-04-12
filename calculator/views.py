fa4bb3739338737aa98acc8b2b220c9623eccd66
Deathik <smertelny@gmail.com>
2017-04-12 14:01:11 +0300

    Changed URLconf, and Year/Month Income views

--- calculator/views.py
+++ calculator/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views.generic import View, ListView, DetailView, YearArchiveView, MonthArchiveView
