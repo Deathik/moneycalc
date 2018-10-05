ff5499bb63979831192c4e24ac09a678bbe7bd88
Sergey Smertelny <smertelny@gmail.com>
2018-10-05 11:28:38 +0300

    Update settings_prod.py

--- moneycalc/settings_prod.py
+++ moneycalc/settings_prod.py
@@ -14,7 +14,7 @@ DATABASES = {
         'HOST': os.getenv('DB_HOST'),
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS),
-        'PORT': os.getenv('DB_PORT),
+        'PASSWORD': os.getenv('DB_PASS'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
