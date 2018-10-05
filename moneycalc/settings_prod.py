0233226240e095ddda44e330a846c8b2ac406ff0
Sergey Smertelny <smertelny@gmail.com>
2018-10-05 11:19:08 +0300

    Update settings_prod.py

--- moneycalc/settings_prod.py
+++ moneycalc/settings_prod.py
@@ -1,18 +1,20 @@
+import os
+
 from moneycalc.settings_dev import *
 
 DEBUG = False
 
 ALLOWED_HOSTS = ['moneycalc.pp.ua']
 
-SECRET_KEY = SECRET_DATA['secret_key']
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'HOST': SECRET_DATA['database']['host'],
-        'NAME': SECRET_DATA['database']['name'],
-        'USER': SECRET_DATA['database']['name'],
-        'PASSWORD': SECRET_DATA['database']['password'],
-        'PORT': SECRET_DATA['database']['port'],
+        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASS),
+        'PORT': os.getenv('DB_PORT),
     }
 }
